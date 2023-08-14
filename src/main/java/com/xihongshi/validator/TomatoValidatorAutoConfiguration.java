c3ac6a565fd9723c6703d848c3fbcdb5c0040f32
MEIYOUx <2232542501@qq.com>
2023-08-14 14:05:24 +0800

    优化：注入默认的验证器工厂Bean

--- src/main/java/com/xihongshi/validator/TomatoValidatorAutoConfiguration.java
+++ src/main/java/com/xihongshi/validator/TomatoValidatorAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.xihongshi.validator;
 
 import com.xihongshi.validator.core.ValidationAspect;
 import com.xihongshi.validator.core.ValidatorFactory;
+import com.xihongshi.validator.core.impl.DefaultValidatorFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,12 @@ public class TomatoValidatorAutoConfiguration {
 
     public TomatoValidatorAutoConfiguration(TomatoValidatorProperties properties) {
         this.properties = properties;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ValidatorFactory validatorFactory() {
+        return new DefaultValidatorFactory();
     }
 
     @Bean
