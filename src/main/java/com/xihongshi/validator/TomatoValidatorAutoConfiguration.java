8399bab7ecbadb48ce0d275cbe2c8ab19d7a8ade
MEIYOUx <2232542501@qq.com>
2023-08-14 13:51:43 +0800

    增加验证切面的注入，用户不需要手动注入该Bean

--- src/main/java/com/xihongshi/validator/TomatoValidatorAutoConfiguration.java
+++ src/main/java/com/xihongshi/validator/TomatoValidatorAutoConfiguration.java
@@ -1,6 +1,10 @@
 package com.xihongshi.validator;
 
+import com.xihongshi.validator.core.ValidationAspect;
+import com.xihongshi.validator.core.ValidatorFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,5 +17,9 @@ public class TomatoValidatorAutoConfiguration {
         this.properties = properties;
     }
 
-
+    @Bean
+    @ConditionalOnMissingBean
+    public ValidationAspect validationAspect(ValidatorFactory factory) {
+        return new ValidationAspect(factory);
+    }
 }
