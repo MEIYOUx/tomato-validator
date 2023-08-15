274f97564bacdb937e3c0ecb831045a47940bd5d
MEIYOUx <2232542501@qq.com>
2023-08-15 12:05:51 +0800

    增加：新增AnnotationException

---
+++ src/main/java/com/xihongshi/validator/exception/AnnotationException.java
@@ -0,0 +1,8 @@
+package com.xihongshi.validator.exception;
+
+public class AnnotationException extends RuntimeException {
+
+    public AnnotationException(String message) {
+        super(message);
+    }
+}
