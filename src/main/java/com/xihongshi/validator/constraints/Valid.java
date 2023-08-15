e6e401d079e6cddc01dc0f08247b852f242babbf
MEIYOUx <2232542501@qq.com>
2023-08-15 12:03:44 +0800

    优化：支持该注解标记方法

--- src/main/java/com/xihongshi/validator/constraints/Valid.java
+++ src/main/java/com/xihongshi/validator/constraints/Valid.java
@@ -2,7 +2,7 @@ package com.xihongshi.validator.constraints;
 
 import java.lang.annotation.*;
 
-@Target(ElementType.PARAMETER)
+@Target({ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Valid {
