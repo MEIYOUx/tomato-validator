f12922d4f7bb03299da3ad352ee1033f040852f0
MEIYOUx <2232542501@qq.com>
2023-08-15 12:03:00 +0800

    修改：1、check方法名称更改为checkAnnotation；2、更改抛出异常的类型

--- src/main/java/com/xihongshi/validator/core/Validator.java
+++ src/main/java/com/xihongshi/validator/core/Validator.java
@@ -1,5 +1,6 @@
 package com.xihongshi.validator.core;
 
+import com.xihongshi.validator.exception.AnnotationException;
 import org.springframework.util.Assert;
 
 import java.lang.annotation.Annotation;
@@ -8,12 +9,11 @@ public interface Validator {
 
     boolean validate(Object object, Annotation annotation);
 
-    default void check(Object object, Annotation annotation, Class<? extends Annotation> type) {
-        Assert.notNull(object, "对象不能为空");
+    default void checkAnnotation(Annotation annotation, Class<? extends Annotation> type) {
         Assert.notNull(annotation, "注解不能为空");
         Assert.notNull(type, "注解类型不能为空");
-        if (!type.isInstance(object)) {
-            throw new IllegalArgumentException(String.format(
+        if (!type.isInstance(annotation)) {
+            throw new AnnotationException(String.format(
                     "注解类型不匹配，需要( %s )，传入( %s )",
                     type.getName(),
                     annotation.annotationType().getName()
